7ac8aeea91e2cfd6ec1f2d5aa9702170a9ee522e
Victor Bueno <victor.bueno0720@gmail.com>
2021-09-09 19:17:03 -0300

    Moved page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head';
+
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello World!</h1>
-  )
+    <>
+      <Head>
+        <title>Home | news.club</title>
+      </Head>
+
+      <h1 className={styles.title}>Hello World!</h1>
+    </>
+  );
 }
