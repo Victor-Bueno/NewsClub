964256419be0fba0a3cb61cee33b222047764167
Victor Bueno <victor.bueno0720@gmail.com>
2021-09-11 10:51:00 -0300

    Created Sign In Button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { SignInBtn } from '../SingInBtn';
+
 import styles from './styles.module.scss';
 
 export function Header() {
@@ -9,6 +11,8 @@ export function Header() {
                     <a className={styles.active}>Home</a>
                     <a>Posts</a>
                 </nav>
+
+                <SignInBtn />
             </div>
         </header>
     )
